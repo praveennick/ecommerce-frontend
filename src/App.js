89be3266741fc2372134782a681098f186ee8e55
praveennick <praveennick1@gmail.com>
2021-03-05 14:45:54 +0530

    added hashrouter and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import Register from './containers/Register';
+import React from 'react'
+import Products from './containers/Products';
+import Login from './containers/Login';
+import Header from './containers/Header';
+import {HashRouter, Route} from 'react-router-dom'
+import ProductList from './containers/ProductList';
+import Dashboard from './containers/Dashboard';
+import AddProduct from './containers/AddProduct';
+import ViewProduct from './containers/ViewProduct';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className="App">
+      <React.Fragment>
+        <HashRouter>
+            <Header/>
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} style={{float:"right"}}/>
+            <Route path="/products" component={Products}/>
+            <Route path="/dashboard" component={Dashboard}/>
+            <Route path="/addProduct" component={AddProduct}/>
+            <Route path="/viewProduct" component={ViewProduct}/>
+            <ProductList/>
+        </HashRouter>
+         
+       
+        {/* <Register/> */}
+        {/* <Login/> */}
+        {/* <Products/> */}
+      </React.Fragment>
+     </div>
   );
 }
 
