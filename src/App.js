1a55f670b584169c6de18d92b653f120a45ed7b2
praveennick <praveennick1@gmail.com>
2021-04-25 14:15:15 +0530

    addedd hashrouter

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import React from 'react'
 import Products from './containers/Products';
 import Login from './containers/Login';
 import Header from './containers/Header';
-import {HashRouter, Route} from 'react-router-dom'
-import ProductList from './containers/ProductList';
+import {HashRouter, Route} from 'react-router-dom';
 import Dashboard from './containers/Dashboard';
 import AddProduct from './containers/AddProduct';
 import ViewProduct from './containers/ViewProduct';
+import AdminProducts from './containers/AdminProducts';
+import DeleteProduct from './containers/DeleteProduct';
+
 
 function App() {
   return (
@@ -17,13 +19,14 @@ function App() {
       <React.Fragment>
         <HashRouter>
             <Header/>
-            <Route path="/login" component={Login} />
+            {/* <Route path="/login" component={Login} />
             <Route path="/register" component={Register} style={{float:"right"}}/>
             <Route path="/products" component={Products}/>
             <Route path="/dashboard" component={Dashboard}/>
             <Route path="/addProduct" component={AddProduct}/>
             <Route path="/viewProduct" component={ViewProduct}/>
-            <ProductList/>
+            <Route path="/admin-products" component={AdminProducts}/>
+            <Route path="/deleteProduct" component={DeleteProduct}/> */}
         </HashRouter>
          
        
