28e939e8d0ddabd03dd3e0762340e8c7839257a4
praveennick <praveennick1@gmail.com>
2021-04-25 14:16:25 +0530

    added url

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+var URL="https://ecommerce-backendapp.herokuapp.com/"
+
 export function registerNewUser(user) {
   alert("Im in register action");
   console.log("user", user);
-  var promise = axios.post("http://localhost:9478/user/register", user);
+  var promise = axios.post(`${URL}user/register`, user);
   console.log("register-promise",promise)
   return {
     type: "REGISTER",
@@ -14,7 +16,7 @@ export function registerNewUser(user) {
 export function loginUser(user){
   console.log("user",user)
   alert("this is login action");
-  var promise= axios.post("http://localhost:9478/user/login",user);
+  var promise= axios.post(`${URL}user/login`,user);
   console.log("login-promise",promise)
   return{
     type:"LOGIN",
