786fc3d1f371ffa2039e436a45c7b8c40337b00a
praveennick <praveennick1@gmail.com>
2021-04-25 14:17:38 +0530

    added dashboard

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import Products from './Products';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import logo from './home.svg';
+// import './Dashboard.css'
 
 class Dashboard extends React.Component{
     
@@ -12,10 +15,211 @@ class Dashboard extends React.Component{
         }
         return(
             <div>
-                <h1>this is Dashboard</h1>
+{/*                 
+
+  <div className="container-fluid ">
+    <div className="row">
+      <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse ">
+        <div className="position-sticky pt-3">
+          <ul className="nav flex-column">
+            <li className="nav-item">
+              <NavLink className="nav-link active" aria-current="page" to="/dashboard">
+                <span data-feather={logo} />
+                Dashboard
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <span data-feather="file" />
+                Orders
+              </a>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/admin-products">
+                <span data-feather="shopping-cart" />
+                Products
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <span data-feather="users" />
+                Customers
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <span data-feather="bar-chart-2" />
+                Reports
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <span data-feather="layers" />
+                Integrations
+              </a>
+            </li>
+          </ul>
+         
+          
+        </div>
+      </nav> */}
+            <h1>this is Dashboard</h1>
                 <NavLink className="nav-link " to="/addProduct">
                     <button type="button" className="btn btn-primary">add product</button>
                 </NavLink>
+                <NavLink className="nav-link " to="/deleteProduct">
+                    <button type="button" className="btn btn-danger">delete product</button>
+                </NavLink>
+       
+                
+
+      {/* <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <h1 className="h2">Dashboard</h1>
+          <div className="btn-toolbar mb-2 mb-md-0">
+            <div className="btn-group me-2">
+              <button type="button" className="btn btn-sm btn-outline-secondary">Share</button>
+              <button type="button" className="btn btn-sm btn-outline-secondary">Export</button>
+            </div>
+            <button type="button" className="btn btn-sm btn-outline-secondary dropdown-toggle">
+              <span data-feather="calendar" />
+              This week
+            </button>
+          </div>
+        </div>
+        <h2>Section title</h2>
+        <div className="table-responsive">
+          <table className="table table-striped table-sm">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Header</th>
+                <th>Header</th>
+                <th>Header</th>
+                <th>Header</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1,001</td>
+                <td>random</td>
+                <td>data</td>
+                <td>placeholder</td>
+                <td>text</td>
+              </tr>
+              <tr>
+                <td>1,002</td>
+                <td>placeholder</td>
+                <td>irrelevant</td>
+                <td>visual</td>
+                <td>layout</td>
+              </tr>
+              <tr>
+                <td>1,003</td>
+                <td>data</td>
+                <td>rich</td>
+                <td>dashboard</td>
+                <td>tabular</td>
+              </tr>
+              <tr>
+                <td>1,003</td>
+                <td>information</td>
+                <td>placeholder</td>
+                <td>illustrative</td>
+                <td>data</td>
+              </tr>
+              <tr>
+                <td>1,004</td>
+                <td>text</td>
+                <td>random</td>
+                <td>layout</td>
+                <td>dashboard</td>
+              </tr>
+              <tr>
+                <td>1,005</td>
+                <td>dashboard</td>
+                <td>irrelevant</td>
+                <td>text</td>
+                <td>placeholder</td>
+              </tr>
+              <tr>
+                <td>1,006</td>
+                <td>dashboard</td>
+                <td>illustrative</td>
+                <td>rich</td>
+                <td>data</td>
+              </tr>
+              <tr>
+                <td>1,007</td>
+                <td>placeholder</td>
+                <td>tabular</td>
+                <td>information</td>
+                <td>irrelevant</td>
+              </tr>
+              <tr>
+                <td>1,008</td>
+                <td>random</td>
+                <td>data</td>
+                <td>placeholder</td>
+                <td>text</td>
+              </tr>
+              <tr>
+                <td>1,009</td>
+                <td>placeholder</td>
+                <td>irrelevant</td>
+                <td>visual</td>
+                <td>layout</td>
+              </tr>
+              <tr>
+                <td>1,010</td>
+                <td>data</td>
+                <td>rich</td>
+                <td>dashboard</td>
+                <td>tabular</td>
+              </tr>
+              <tr>
+                <td>1,011</td>
+                <td>information</td>
+                <td>placeholder</td>
+                <td>illustrative</td>
+                <td>data</td>
+              </tr>
+              <tr>
+                <td>1,012</td>
+                <td>text</td>
+                <td>placeholder</td>
+                <td>layout</td>
+                <td>dashboard</td>
+              </tr>
+              <tr>
+                <td>1,013</td>
+                <td>dashboard</td>
+                <td>irrelevant</td>
+                <td>text</td>
+                <td>visual</td>
+              </tr>
+              <tr>
+                <td>1,014</td>
+                <td>dashboard</td>
+                <td>illustrative</td>
+                <td>rich</td>
+                <td>data</td>
+              </tr>
+              <tr>
+                <td>1,015</td>
+                <td>random</td>
+                <td>tabular</td>
+                <td>information</td>
+                <td>text</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </main> */}
+
+                
+            {/* <Products/> */}
+
             </div>
         )
     }
