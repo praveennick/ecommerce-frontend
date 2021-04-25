10a30de414747ee430f708b1b71794bd71d26efa
praveennick <praveennick1@gmail.com>
2021-04-25 14:19:50 +0530

    addded prducts.js

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -3,64 +3,72 @@ import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { bindActionCreators } from "redux";
 import {getAllProducts} from '../actions/productsActions';
-import ProductList from "./ProductList";
 import './Products.css';
-import {viewProductById} from '../actions/productsActions'
+import {viewProductById} from '../actions/productsActions';
 
 class Products extends React.Component {
   constructor(){
     super();
     this.state={
     }
-    this.renderList=this.renderList.bind(this);
+    // this.renderList=this.renderList.bind(this);
   }
 
   componentDidMount(){
+    console.log("this is componentdidmount")
     this.props.getAllProducts()
   }
-  renderList(){
-    // var listitems=this.props.products.map(
-    //   (product)=>{
-    //     return (this.setState({productListItems:product}))
-    //   })
-    //   return listitems;
-  }
+  
+  // renderList(){
+  //   this.props.getAllProducts()
+  // }
   render() {
+    // console.log("in dashboard products",this.props.isUserLoggedIn)
+    // if(!this.props.isUserLoggedIn){
+    //     alert("Please login");
+    //     this.props.history.push('/login')
+    // }
+    console.log("rendered",this.props.allProducts.length)
     return (
       <div>
-          {console.log("object products",this.props.products)}
-          {this.props.products.map(
+          {console.log("object products",this.props.allProducts)}
+          
+          {this.props.allProducts.map(
 
             (product)=>{
                     return (
 
-                      <div className="row">
+                      <div className="row" key={product.pid}>
   <div className="col-lg-8 mx-auto">
     {/* List group*/}
-    <ul className="list-group shadow" >
+    <ul className="list-group shadow">
       {/* list group item*/}
-      <li className="list-group-item" key={product.pid} >
+      <li className="list-group-item">
         {/* Custom content*/}
         <div className="media align-items-lg-center flex-column flex-lg-row p-3">
           <div className="media-body order-2 order-lg-1">
             <h5 className="mt-0 font-weight-bold mb-2">{product.product_name}</h5>
+            <h6 className="d-inline text-secondary small">{product.category} {">"} </h6><h6 className="small d-inline text-secondary">{product.sub_category}</h6>
             <p className="font-italic text-muted mb-0 small">{product.product_specifications}</p>
             <div className="d-flex align-items-center justify-content-between mt-1">
               <h6 className="font-weight-bold my-2">₹{product.price}</h6>
               
             </div>
             <button type="button" className="btn btn-primary">add</button>
-            <NavLink className="nav-link"
+            <button className="nav-link"
              onClick={
                (event)=>{
                  event.preventDefault();
                  console.log("object clicked");
-                 this.props.viewProductById(product.pid);
+                //  this.props.viewProductById(product.pid);
+                localStorage.setItem("pid",product.pid)
                  this.props.history.push("/viewProduct")
                 }
                 }
-                to="/viewProduct" type="button" className="btn btn-success" productId={product.pid}>view</NavLink>
+                className="btn btn-success" >view</button>
             
+          {/* <button type="button" className="btn btn-danger">delete</button> */}
+
           </div><img src={product.product_image} alt="Generic placeholder image" className="ml-lg-5 order-1 order-lg-2" />
         </div> {/* End */}
       </li> {/* End */}
@@ -72,7 +80,7 @@ class Products extends React.Component {
 
             }
           )}
-        {console.log("products in",typeof (this.props.products))}
+        {console.log("products in",typeof (this.props.allProducts))}
           {/* <h1>{this.props.products.length}</h1> */}
       </div>
     );
@@ -82,7 +90,8 @@ class Products extends React.Component {
 function mapStateToProps(appState){
   console.log(appState);
  return {
-   products: appState.products,
+  allProducts: appState.allProducts
+  //  isUserLoggedIn:appState.isUserLoggedIn
   }
 }
 
